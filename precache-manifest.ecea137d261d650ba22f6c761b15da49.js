3b341fd0cf5ccac7186545844b2c6dd668d15e3d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-15 23:07:24 +0000

    Deploy yurymuski/resume to github.com/yurymuski/resume.git:gh-pages

---
+++ precache-manifest.ecea137d261d650ba22f6c761b15da49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab027453368dba40130c5a89af3cf5c",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "cffcfcf3865ee6a2a473",
+    "url": "/resume/static/css/2.f4948e68.chunk.css"
+  },
+  {
+    "revision": "d8b2d5b26f3fc4fcae55",
+    "url": "/resume/static/css/main.4e6efbae.chunk.css"
+  },
+  {
+    "revision": "cffcfcf3865ee6a2a473",
+    "url": "/resume/static/js/2.f6def034.chunk.js"
+  },
+  {
+    "revision": "d8b2d5b26f3fc4fcae55",
+    "url": "/resume/static/js/main.4a6e0bba.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
