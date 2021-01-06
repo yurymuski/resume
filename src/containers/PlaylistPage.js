66301ede58be62c09ae722e0b046bb1d2154e6c5
yurymuski <muski.yury@gmail.com>
2021-01-07 02:51:24 +0300

    fix playlist video fullscreen

--- src/containers/PlaylistPage.js
+++ src/containers/PlaylistPage.js
@@ -7,7 +7,7 @@ export default ({ playlist }) => (
       Tech talks
     </p>
     <div className="video-container">
-      <iframe src="https://www.youtube.com/embed/videoseries?list=PLW-NZsw8bcdJQEcYyTEQNTTnY775l_yW0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen title="Tech talks" className="video">
+      <iframe src="https://www.youtube.com/embed/videoseries?list=PLW-NZsw8bcdJQEcYyTEQNTTnY775l_yW0" frameborder="0" allow="autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen" allowfullscreen title="Tech talks" className="video">
       </iframe>
     </div>
   </div>
