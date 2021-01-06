c651e4e174aa167b09f2ed3b8bd153bb5d4e0d1c
yurymuski <muski.yury@gmail.com>
2021-01-07 02:38:18 +0300

    fix playlist video Width

--- src/containers/PlaylistPage.js
+++ src/containers/PlaylistPage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./PlaylistPage.css";
 
 export default ({ playlist }) => (
   <div>
     <p className="headline" style={{ color: "Black" }}>
       Tech talks
     </p>
-      <iframe width="640" height="360" src="https://www.youtube.com/embed/videoseries?list=PLW-NZsw8bcdJQEcYyTEQNTTnY775l_yW0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen title="Tech talks">
+    <div className="video-container">
+      <iframe src="https://www.youtube.com/embed/videoseries?list=PLW-NZsw8bcdJQEcYyTEQNTTnY775l_yW0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen title="Tech talks" className="video">
       </iframe>
+    </div>
   </div>
 );
